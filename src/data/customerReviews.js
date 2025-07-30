d4f84fe996b5a371cb382f449feba8cd4dd793bb
Sarath TV <sarathmithran99@gmail.com>
2025-07-30 12:45:20 +0530

    updated CustomerReview component

---
+++ src/data/customerReviews.js
@@ -0,0 +1,22 @@
+const reviews = [
+    {
+      name: "Sarah Johnson",
+      rating: 5,
+      review:
+        "Best burgers in town! The beef is so juicy and the fries are perfectly crispy. Customer service is amazing too!",
+    },
+    {
+      name: "Mike Chen",
+      rating: 5,
+      review:
+        "I'm a regular here and they never disappoint. The veggie burger is surprisingly delicious - even my meat-loving friends love it!",
+    },
+    {
+      name: "Emma Rodriguez",
+      rating: 5,
+      review:
+        "Perfect spot for family dinner. Kids love the mini burgers and the staff is so friendly and accommodating.",
+    },
+];
+
+export { reviews };
