39fa1f537899fb1d7c0370b55fb6b9893e0247bd
Sarath TV <sarathmithran99@gmail.com>
2025-08-01 22:55:20 +0530

    updated home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import HeroSection from '../../components/heroSection/HeroSection'
-import FeaturedSection from '../../components/featuredSection/FeaturedSection'
 import MenuSection from '../../components/menuSection/MenuSection'
 import CustomerReview from '../../components/customerReview/CustomerReview'
 import StatsSection from '../../components/statsSection/StatsSection'
@@ -10,7 +9,6 @@ const Home = () => {
     <>
         <HeroSection/>
         <MenuSection/>
-        {/* <FeaturedSection/> */}
         <StatsSection/>
         <CustomerReview/>
     </>
